d224fd2fc95f26711613e03929d2a3b167f57977
tampt <tampt95@gmail.com>
2021-10-24 13:20:44 +0700

    assets management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import * as _ from "lodash";
+import "./style.css";
+import MyImage from "./image.jpg";
+import Data from './data.xml'
+import Notes from './data.csv'
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const image = new Image();
+  image.src = MyImage;
+
+  element.appendChild(image);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
